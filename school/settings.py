13d3b6ec1f6406fba7776a5e3b4427f5ffea42f5
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-13 14:01:16 +0300

    Adds configuration so that the templates may load media files

--- school/settings.py
+++ school/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_school.apps.DjangoSchoolConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -126,3 +128,11 @@ AUTH_USER_MODEL = 'django_school.User'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
