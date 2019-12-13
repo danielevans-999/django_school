a2831e00e3cfaa5fb7018285e3a3a93dec96f9fe
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-13 13:48:05 +0300

    migtrates tto upgrade to the database changes.

--- django_school/admin.py
+++ django_school/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . models import User
+from . forms import *
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    form = UserchangeForm
+    add_form = UserCreationForm
+    
+    list_display = ('email', 'user_type','username', 'is_admin')
+    list_filter = ('is_admin', )
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('user_type',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_type','username', 'password1', 'password2'),
+        }),
+    )
+    
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+    
+admin.site.register(User, UserAdmin)
+
+admin.site.unregister(Group)
+
+
--- school/settings.py
+++ school/settings.py
@@ -117,6 +117,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# custom authentications
+
+AUTH_USER_MODEL = 'django_school.User'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
