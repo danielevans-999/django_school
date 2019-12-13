6f6fea562a04a5c984f190118ed014b51cbfd866
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-13 08:46:15 +0300

    Adds the tempates directory to be available anywhere within the folder

--- school/settings.py
+++ school/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_school.apps.DjangoSchoolConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'school.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
