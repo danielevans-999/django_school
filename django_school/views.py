e957b56bc08bc1c2e0d471951fdfa26049b2b6d1
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-15 10:40:31 +0300

    Creates the teachers sign up view.

--- django_school/views.py
+++ django_school/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.views.generic import CreateView
 from . models import User
-from . forms import UserCreationForm
+from . forms import (StudentSignUpForm, TeachersSignUpForm)
 
 def index(request):
     return render(request, 'djangoschool/index.html')
@@ -9,7 +9,7 @@ def index(request):
 class StudentSignup(CreateView):
     
     model = User
-    form_class = UserCreationForm
+    form_class = StudentSignUpForm
     template_name = 'registration/signup_form.html'
     
     def get_context_data(self, **kwargs):
@@ -19,3 +19,17 @@ class StudentSignup(CreateView):
     def form_valid(self, form):
         user = form.save()
         return redirect('home')
+    
+class TeacherSignUpView(CreateView):
+    model = User
+    form_class = TeachersSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'Teacher'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        return redirect('home')
+        
