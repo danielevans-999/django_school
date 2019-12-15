754d46ed91ac66f1a882be555ff3eafdb38bd1ed
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-15 07:26:15 +0300

    Creates student signup view

--- django_school/views.py
+++ django_school/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import CreateView
+from . models import User
+from . forms import UserCreationForm
 
 def index(request):
     return render(request, 'djangoschool/index.html')
+
+class StudentSignup(CreateView):
+    
+    model = User
+    form_class = UserCreationForm
+    template_name = 'registration/signup_form'
+    
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 1
+        return super().get_context_data(**kwargs)
+    
+    def form_valid(self, form):
+        user = form.save()
+        return redirect('home')
