acc5ca19677ec43df58171849dbf01a54cef52ec
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-16 14:02:46 +0300

    Adds the profile view

--- django_school/views.py
+++ django_school/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render,redirect
 from django.views.generic import CreateView
-from . models import User
-from . forms import (StudentSignUpForm, TeachersSignUpForm)
+from . models import *
+from . forms import (StudentSignUpForm, TeachersSignUpForm, StudentProfileForm)
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 def index(request):
+    if request.user.is_authenticated:
+        if request.user.user_type == 1:
+            print('Hello teacher')
+        else:
+            return redirect('student_profile')
     return render(request, 'djangoschool/index.html')
 
 class StudentSignup(CreateView):
@@ -18,6 +26,7 @@ class StudentSignup(CreateView):
     
     def form_valid(self, form):
         user = form.save()
+        login(self.request, user)
         return redirect('home')
     
 class TeacherSignUpView(CreateView):
@@ -31,5 +40,37 @@ class TeacherSignUpView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        login(self.request, user)
         return redirect('home')
-        
+    
+def student_profile_info(request):
+    
+    current_user=request.user
+    profile_info = StudentProfile.objects.filter(user=current_user).first()
+    return render(request,'djangoschool/student-profile.html',{"profile":profile_info,"current_user":current_user})
+
+def student_profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = StudentProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('student_profile')
+
+    else:
+        form = StudentProfileForm()
+        return render(request,'djangoschool/edit.html',{"form":form})
+    
+def update_profile(request):
+    user_profile = StudentProfile.objects.get(user=request.user)
+    
+    if request.method == "POST":
+        form =  StudentProfileForm(request.POST,request.FILES,instance=request.user.studentprofile)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = StudentProfileForm(instance=request.user.studentprofile)
+        return render(request,'djangoschool/edit.html',{"form":form})
